407def4ffa3c51229be926218c3da7a3ae290cea
misunders2d <2djohar@gmail.com>
2026-08-17 16:38:01 +0300

    fix(nvim): resolve project imports with gf

--- nvim/.config/nvim/lua/autocmds.lua
+++ nvim/.config/nvim/lua/autocmds.lua
@@ -5,3 +5,25 @@ vim.api.nvim_create_autocmd("TextYankPost", {
 		vim.hl.on_yank()
 	end,
 })
+
+vim.api.nvim_create_autocmd("FileType", {
+	desc = "Add project roots to file lookup",
+	group = vim.api.nvim_create_augroup("user-project-path", { clear = true }),
+	pattern = "*",
+	callback = function(args)
+		local root = vim.fs.root(
+			args.buf,
+			{ ".git", "pyproject.toml", "package.json", "Cargo.toml", "go.mod", "setup.py", "setup.cfg" }
+		)
+		if not root then
+			return
+		end
+
+		vim.opt_local.path:append(root)
+		local src = vim.fs.joinpath(root, "src")
+		local stat = vim.uv.fs_stat(src)
+		if stat and stat.type == "directory" then
+			vim.opt_local.path:append(src)
+		end
+	end,
+})
--- nvim/.config/nvim/lua/keymaps.lua
+++ nvim/.config/nvim/lua/keymaps.lua
@@ -18,6 +18,7 @@ map("n", "]b", ":bnext<CR>", { desc = "Next buffer" })
 map("n", "[b", ":bprevious<CR>", { desc = "Prev buffer" })
 
 map("n", "<leader>q", vim.diagnostic.setloclist, { desc = "Diagnostics to loclist" })
+map("n", "<leader>gf", "gf", { desc = "Go to file under cursor" })
 
 map("i", "<C-v>", "<C-r>+", { desc = "Paste clipboard inline" })
 map("c", "<C-v>", "<C-r>+", { desc = "Paste clipboard inline" })
